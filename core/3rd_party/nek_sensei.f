324f5ad3be5f526f46e6f9259eff2c60f74d5553
Bennett Bernardoni <bbernardoni@anl.gov>
2018-07-25 17:09:57 +0000

    refactored nek_sensei build process

--- core/3rd_party/nek_sensei.f
+++ core/3rd_party/nek_sensei.f
@@ -5,8 +5,10 @@ c-----------------------------------------------------------------------
       include 'TOTAL'
       common /nekmpi/ mid,mp,nekcomm,nekgroup,nekreal
 
-      call sensei_bridge_initialize(nekcomm,
-     &       lx1, ly1, lz1, nelt, xm1, ym1, zm1)
+      call sensei_bridge_initialize(nekcomm, CASENAME//char(0), nelt,
+     &       lx1, ly1, lz1, xm1, ym1, zm1,
+     &       lx2, ly2, lz2, xm2, ym2, zm2,
+     &       vx, vy, vz, pr)
 #endif
       end
 c-----------------------------------------------------------------------
@@ -17,7 +19,7 @@ c-----------------------------------------------------------------------
 c      common /scrcg/ pm1(lx1,ly1,lz1,lelv)
 
 c Might need pm1 instead of pr
-      call sensei_bridge_update(istep, time, vx, vy, vz, pr)
+      call sensei_bridge_update(istep, time)
 #endif
       end
 c-----------------------------------------------------------------------
